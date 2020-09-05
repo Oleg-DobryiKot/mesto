85d558065f7e6ae4820f98f012b385ad21592423
Oleg Matveev <matveev@optshintorg.by>
2020-09-05 13:25:06 +0300

    Make mistake control to second rev. v.1.3.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,35 +2,41 @@ const popup = document.querySelector('.popup');
 const popupEditButton = document.querySelector('.profile__edit-btn');
 const popupCloseButton = document.querySelector('.popup__close');
 const popupSaveButton = document.querySelector('.popup__input-btn');
-const SaveButton = document.querySelector('.popup__input-btn_save');
 const profileNewName = document.querySelector('.profile__title');
-const profileNewDescription = document.querySelector('.profile__description')
+const profileNewDescription = document.querySelector('.profile__description');
+const inputName = document.querySelector('.popup__input-text_name');
+const inputDescription = document.querySelector('.popup__input-text_description');
 
-const popupToggle = function () {
-  popup.classList.toggle('popup_is-opened');
+const popupOpen = function () {
+  popup.classList.add('popup_is-opened');
 }
 
-const ClosePopupOnOverspace = function () {
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-  popupToggle();
+const popupClose = function () {
+  popup.classList.remove('popup_is-opened');
 }
 
-function SaveNewData() {
-  let name = document.querySelector('.popup__input-text_name');
-  let description = document.querySelector('.popup__input-text_description');
-  
-  if (name !== '') {
-    profileNewName.textContent = name.value;
-    profileNewDescription.textContent = description.value;
+const popupCloseOnOverspace = function (event) {
+  if (event.target === event.currentTarget) {
+    popupClose();
   }
+}
+
+function popupSaveNewData() {
+  if (inputName.value !== '') {
+    profileNewName.textContent = inputName.value;
+    profileNewDescription.textContent = inputDescription.value;
+  }
+  popupClose();
   return;
 }
 
-SaveButton.addEventListener('click', SaveNewData);
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  popupSaveNewData();
+});
 
-popupEditButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-popupSaveButton.addEventListener('click', popupToggle);
-popup.addEventListener('click', ClosePopupOnOverspace);
+popupEditButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
+
+popup.addEventListener('click', popupCloseOnOverspace);
