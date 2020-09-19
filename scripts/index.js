5144233f340afadd1c35c732e9d96668447f2fae
Oleg Matveev <matveev@optshintorg.by>
2020-09-19 19:35:12 +0300

    Make basic add-function control v.5.1.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,64 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const elementsContainer = document.querySelector('.elements');
+const elemCardContainer = document.querySelector('#elem__container').content;
+
+function prepareElement(elementModel) {
+  const element = elemCardContainer.cloneNode(true);
+  if (elementModel.link) {
+    element.querySelector('.element__image').src = elementModel.link;
+  }
+  if (elementModel.name) {
+    element.querySelector('.element__title').textContent = elementModel.name;
+  }
+  element.querySelector('.del').addEventListener('click', () => {
+    debugger;
+    element.remove();
+  });
+
+  return element;
+}
+
+function appendElement(elementModel) {
+  const element = prepareElement(elementModel);
+  elementsContainer.append(element);
+}
+
+function prependElement(elementModel) {
+  const element = prepareElement(elementModel);
+  elementsContainer.prepend(element);
+}
+
+// const forEach = (arr, cb) => Array.prototype.forEach.call(arr, cb);
+
+initialCards.forEach(appendElement);
+
+/*
 const popup = document.querySelector('.popup');
 const popupEditButton = document.querySelector('.profile__edit-btn');
 const popupCloseButton = document.querySelector('.popup__close');
@@ -8,8 +69,6 @@ const inputName = document.querySelector('.popup__input-text_name');
 const inputDescription = document.querySelector('.popup__input-text_description');
 const form = document.querySelector("form");
 
-console.log(profileNewName.textContent);
-
 const popupOpen = function () {
   inputName.value = profileNewName.textContent;
   inputDescription.value = profileNewDescription.textContent;
@@ -19,7 +78,7 @@ const popupOpen = function () {
 const popupClose = function () {
   popup.classList.remove('popup_is-opened');
 }
-
+ 
 const popupCloseOnOverspace = function (event) {
   if (event.target === event.currentTarget) {
     popupClose();
@@ -41,3 +100,4 @@ popupEditButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
 
 popup.addEventListener('click', popupCloseOnOverspace);
+*/
