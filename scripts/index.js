871ca3bf820a28a50437200f453e4f6ef5e99fe4
Oleg Matveev <matveev@optshintorg.by>
2020-09-20 13:35:17 +0300

    Make image-form v.5.3.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
+const popup = document.querySelector('.popup');
+const popupEditButton = document.querySelector('.profile__edit-btn');
+const popupAddButton = document.querySelector('.profile__add-btn');
+const popupCloseButton = document.querySelector('.popup__close');
+const popupSaveButton = document.querySelector('.popup__input-btn');
+const profileNewName = document.querySelector('.profile__title');
+const profileNewDescription = document.querySelector('.profile__description');
+const inputName = document.querySelector('.popup__input-text_name');
+const inputDescription = document.querySelector('.popup__input-text_description');
+const formName = document.querySelector('.popup__title');
+const form = document.querySelector("form");
+const elementsContainer = document.querySelector('.elements');
+const elemCardContainer = document.querySelector('#elem__container').content;
 const initialCards = [
   {
     name: 'Архыз',
@@ -25,9 +38,6 @@ const initialCards = [
   }
 ];
 
-const elementsContainer = document.querySelector('.elements');
-const elemCardContainer = document.querySelector('#elem__container').content;
-
 function prepareElement(elementModel) {
   const element = elemCardContainer.cloneNode(true);
   if (elementModel.link) {
@@ -39,10 +49,6 @@ function prepareElement(elementModel) {
   element.querySelector('.element__like-icon').addEventListener('click', function (event) {
     event.target.classList.toggle('element__like-icon_active');
   });
-  // element.querySelector('.element__trash-icon').addEventListener('click', () => {
-  //   debugger;
-  //   element.remove();
-  // });
   return element;
 }
 
@@ -58,26 +64,26 @@ function prependElement(elementModel) {
 
 initialCards.forEach(appendElement);
 
-const popup = document.querySelector('.popup');
-const popupEditButton = document.querySelector('.profile__edit-btn');
-const popupAddButton = document.querySelector('.profile__add-btn');
-const popupCloseButton = document.querySelector('.popup__close');
-const popupSaveButton = document.querySelector('.popup__input-btn');
-const profileNewName = document.querySelector('.profile__title');
-const profileNewDescription = document.querySelector('.profile__description');
-const inputName = document.querySelector('.popup__input-text_name');
-const inputDescription = document.querySelector('.popup__input-text_description');
-const form = document.querySelector("form");
+elementsContainer.addEventListener('click', (event) => {
+  if (event.target.classList.contains('element__trash-icon')) {
+    const element = event.target.closest('.element');
+    element.remove();
+  }
+});
 
 const popupOpen = function (event) {
   if (event.target.classList.value === 'profile__edit-btn') {
   //console.log(event.target.classList.value);
+    formName.textContent = 'Редактировать профиль'
     inputName.value = profileNewName.textContent;
     inputDescription.value = profileNewDescription.textContent;
-  }
-  if (event.target.classList.value === 'profile__add-btn') {
-    inputName.value = 'test-text';
-    inputDescription.value = profileNewDescription.textContent;
+    form.addEventListener("submit", popupSaveNewData);
+  } else {
+  //if (event.target.classList.value === 'profile__add-btn') {
+    formName.textContent = 'Новое место'
+    inputName.value = 'Название';
+    inputDescription.value = 'Ссылка на картинку';
+    form.addEventListener("submit", popupSaveNewElement);
   }
   popup.classList.add('popup_is-opened');
 }
@@ -101,9 +107,17 @@ function popupSaveNewData() {
   popupClose();
 }
 
-form.addEventListener("submit", popupSaveNewData);
+function popupSaveNewElement(elementModel) {
+  event.preventDefault();
+  if (inputName.value !== '') {
+    elementModel.link = inputName.value;
+    elementModel.name = inputDescription.value;
+    prependElement(elementModel);
+  }
+  popupClose();
+}
 
 popupEditButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
-popupAddButton.addEventListener('click', popupOpen);
 popup.addEventListener('click', popupCloseOnOverspace);
+popupAddButton.addEventListener('click', popupOpen);
