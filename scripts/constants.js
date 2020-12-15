3215ff805b4b8232cfd80760928bd440a06a326c
Oleg Matveev <matveev@optshintorg.by>
2020-12-15 15:11:38 +0300

    make constants for second rev v7.19.rev2

---
+++ scripts/constants.js
@@ -0,0 +1,67 @@
+// карточки из кардс чтоб три файла как в задании.
+export const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+export const initValidationConfig = {
+  formSelector: '.popup__input-form',
+  inputSelector: '.popup__input-text',
+  submitButtonSelector: '.popup__input-btn',
+  inputErrorClass: 'popup__input-text_type_error',
+  errorSelector: '.popup__error',
+  errorClass: 'popup__error_visible'
+};
+
+// попап редактирования профиля -- определения
+export const profilePopup = document.querySelector('.popup_type-profile');
+export const cardPopup = document.querySelector('.popup_type-card');
+export const imgPopup = document.querySelector('.popup_type-image');
+
+//const profileSaveButton = document.querySelector('.popup__input-btn_type-profile');
+export const profileInputName = document.querySelector('.popup__input-text_name');
+export const profileInputDescription = document.querySelector('.popup__input-text_description');
+
+export const cardInputName = document.querySelector('.popup__input-text_title');
+export const cardInputLink = document.querySelector('.popup__input-text_link');
+//const cardSaveButton = document.querySelector('.popup__input-btn_type-card');
+
+// попап всплывающей картинки -- определения 
+export const imgPictureLoad = document.querySelector('.popup__fullpic');
+export const imgTitleLoad = document.querySelector('.popup__title_type-image');
+
+// кнопки профиля, редактирование и добавление
+export const popupEditButton = document.querySelector('.profile__edit-btn');
+export const cardAddButton = document.querySelector('.profile__add-btn');
+
+//const popupCloseButton = document.querySelector('.popup__close');
+
+export const profileNewName = document.querySelector('.profile__title');
+export const profileNewDescription = document.querySelector('.profile__description');
+
+export const formCard = document.forms.inputFormCard;
+export const formProfile = document.forms.inputFormProfile;
+
+export const elementsContainer = document.querySelector('.elements');
+export const cardTemplate = document.querySelector('#cardTemplate').content;
--- scripts/index.js
+++ scripts/index.js
@@ -1,75 +1,26 @@
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
-
-// перекинуть бы все определяшки в какой-нить инишиал.жис как в тренажере.
-
-// карточки из кардс чтоб три файла как в задании.
-const initialCards = [{
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-const initValidationConfig = {
-  formSelector: '.popup__input-form',
-  inputSelector: '.popup__input-text',
-  submitButtonSelector: '.popup__input-btn',
-  inputErrorClass: 'popup__input-text_type_error',
-  errorSelector: '.popup__error',
-  errorClass: 'popup__error_visible'
-};
-
-// попап редактирования профиля -- определения
-const profilePopup = document.querySelector('.popup_type-profile');
-const cardPopup = document.querySelector('.popup_type-card');
-const imgPopup = document.querySelector('.popup_type-image');
-
-//const profileSaveButton = document.querySelector('.popup__input-btn_type-profile');
-const profileInputName = document.querySelector('.popup__input-text_name');
-const profileInputDescription = document.querySelector('.popup__input-text_description');
-
-const cardInputName = document.querySelector('.popup__input-text_title');
-const cardInputLink = document.querySelector('.popup__input-text_link');
-//const cardSaveButton = document.querySelector('.popup__input-btn_type-card');
-
-// попап всплывающей картинки -- определения 
-const imgPictureLoad = document.querySelector('.popup__fullpic');
-const imgTitleLoad = document.querySelector('.popup__title_type-image');
-
-// кнопки профиля, редактирование и добавление
-const popupEditButton = document.querySelector('.profile__edit-btn');
-const cardAddButton = document.querySelector('.profile__add-btn');
-
-//const popupCloseButton = document.querySelector('.popup__close');
-
-const profileNewName = document.querySelector('.profile__title');
-const profileNewDescription = document.querySelector('.profile__description');
-
-//const formElements = document.forms.inputFormCard;
-const formProfile = document.forms.inputFormProfile;
-
-const elementsContainer = document.querySelector('.elements');
-const cardTemplate = document.querySelector('#cardTemplate').content;
+import {
+  initialCards,
+  initValidationConfig,
+  profilePopup,
+  cardPopup,
+  imgPopup,
+  profileInputName,
+  profileInputDescription,
+  cardInputName,
+  cardInputLink,
+  imgPictureLoad,
+  imgTitleLoad,
+  popupEditButton,
+  cardAddButton,
+  profileNewName,
+  profileNewDescription,
+  formCard,
+  formProfile,
+  elementsContainer,
+  cardTemplate,
+} from './constants.js';
 
 function openPopupImage(cardElement) {
   imgPictureLoad.src = cardElement.link;
@@ -113,13 +64,6 @@ function openPopup(popup) {
   }
 
   popup.addEventListener('click', closeHandler);
-
-  // function closeCurrentPopup() {
-  //   handleClosePopup(popup);
-  //   popupCloseButton.removeEventListener('click', closeCurrentPopup);
-  // };
-
-  // popupCloseButton.addEventListener('click', closeCurrentPopup);
 }
 
 function closePopup(popup, popupClickHandler) {
@@ -127,17 +71,6 @@ function closePopup(popup, popupClickHandler) {
   document.removeEventListener("keydown", closeByEscape);
   popup.removeEventListener('click', popupClickHandler);
 }
-
-// function handleClosePopup(popup) {
-//   return function (event) {
-//     if (event.target === event.currentTarget) {
-//       closePopup(popup);
-//     }
-//     if (event.target.classList.contains("popup__close")) {
-//       closePopup(popup);
-//     }
-//   }
-// }
 
 function closeByEscape(event) {
   if (event.key === "Escape") {
@@ -176,13 +109,8 @@ formProfile.addEventListener("submit", event => {
   savePopupProfileChanges();
 });
 
-// profilePopup.addEventListener('click', handleClosePopup(profilePopup));
-// cardPopup.addEventListener('click', handleClosePopup(cardPopup));
-// imgPopup.addEventListener('click', handleClosePopup(imgPopup));
-
 cardAddButton.addEventListener('click', () => {
-  cardInputLink.value = '';
-  cardInputName.value = '';
+  formCard.reset();
   openPopup(cardPopup);
 });
 
