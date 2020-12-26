3a95c47aef814b391b632f57326c2ddd40737cfb
Oleg Matveev <matveev@optshintorg.by>
2020-12-26 22:29:02 +0300

    tuning webpack  v8.19.dev.4

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 const path = require('path'); // подключаем path к конфигу вебпак
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плаг 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 
-  entry: { main: './src/pages/index.js' },
+  entry: {
+    main: './src/pages/index.js'
+  },
 
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     publicPath: ''
-    },
-  
+  },
+
   mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
@@ -21,36 +25,50 @@ module.exports = {
 
     open: true // сайт будет открываться сам при запуске npm run dev
   },
-  
-  module: {
-    rules: [
-      {
-      test: /\.js$/,
-      use: 'babel-loader',
-      exclude: '/node_modules/'
-      },
-  // добавили правило для обработки файлов
-      {
-    // регулярное выражение, которое ищет все файлы с такими расширениями
-      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-      type: 'asset/resource'
-      },
-      {
-    // применять это правило только к CSS-файлам
-      test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, {
-        loader: 'css-loader',
-        options: { importLoaders: 1 }
-        },
-        'postcss-loader'
-      ]
-      }
-    ] 
-  },
 
+  module: {
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      // добавили правило для обработки файлов
+      {
+        // регулярное выражение, которое перекладывает картинки в /имиджс
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[contenthash][ext]',
+        }
+      },
+      {
+        // регулярное выражение, которое перекладывает шрифты в папку /фонтс
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[contenthash][ext]',
+        }
+      },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  devtool: "source-map",
   plugins: [
-    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
-  ] 
-}; 
+  ]
+};
